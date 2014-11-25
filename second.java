5061c48bd52bcf2aaf4b18c12168a491e988624a
my acount <fgmolina@gmail.com>
2014-11-25 14:18:03 -0200

    nuevo Soma

--- second.java
+++ second.java
@@ -7,9 +7,17 @@ public class second {
 	    /*
 	     * Declaracao de variaveis.
 	     * Declara uma variavel de nome "num" do tipo inteiro
+	     * O valor inicial da variavel eh 0
 	     */
 	    int num = 0;
+	    int n   = 0;
 
+	    /*
+	     * Declaracao de variaveis que recebe o valor da soma.
+	     * Declara uma variavel de nome "sum" do tipo inteiro
+	     * O valor inicial da variavel eh 0
+	     */
+	    int sum = 0;
 	    /*
 	     * This function writes a phrase in the java console
 	     */
@@ -25,7 +33,7 @@ public class second {
 	     */
 	    System.out.println("Valor de num: "+num);
 
-
+	    
 
 
 	}
