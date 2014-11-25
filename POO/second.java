7bf82ef30310c3d7a0afbb676edbdb09fa39b2ae
my acount <fgmolina@gmail.com>
2014-11-25 13:38:59 -0200

    Segundo fichero

---
+++ POO/second.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class second {
+
+	public static void main(String[] args) {
+
+	    /*
+	     * Declaracao de variaveis.
+	     * Declara uma variavel de nome "num" do tipo inteiro
+	     */
+	    int num = 0;
+
+	    /*
+	     * This function writes a phrase in the java console
+	     */
+	    System.out.println("Hello world");
+
+	    /*
+	     * Atribue um valor aa variavel num.
+	     */
+	    num = 9;
+
+	    /*
+	     * Imprime o valor da variavel num.
+	     */
+	    System.out.println("Valor de num: "+num);
+
+
+
+
+	}
+}
