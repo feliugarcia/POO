233cd7fbb15e2697a32e0a77b659e4c2237cf607
my acount <fgmolina@gmail.com>
2014-12-03 09:59:08 -0200

    Objeto Java

---
+++ Objeto/Banco.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Banco {
+
+
+    public static void main(String[] args) {
+	
+	System.out.println("System");
+
+	Soma s = new Soma();
+
+	System.out.println("Resultado Soma = "+s.getResultado());
+	System.out.println(" Op1 = "+ s.getOp1());
+
+	Soma ss = new Soma(-9,18);
+
+	System.out.println("Resultado Soma = "+ss.getResultado());
+	System.out.println(" Op1 = "+ ss.getOp1());
+
+	Subtracao sb = new Subtracao(-9,18);
+
+	System.out.println("Resultado Subtracao = "+sb.getResultado());
+	System.out.println(" Op1 = "+ sb.getOp1());
+
+    }
+
+}
